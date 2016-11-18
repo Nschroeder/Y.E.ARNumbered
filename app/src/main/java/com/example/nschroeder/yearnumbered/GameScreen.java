ae82192d3d48ecb2e24a54c9df7bef1e1d1994f8
nschroeder <nschroeder@gips.org>
2016-11-18 13:50:38 -0600

    Changed former idea, working on creating buttons to move an image left/right

--- app/src/main/java/com/example/nschroeder/yearnumbered/GameScreen.java
+++ app/src/main/java/com/example/nschroeder/yearnumbered/GameScreen.java
@@ -2,43 +2,47 @@ package com.example.nschroeder.yearnumbered;
 
 import android.content.Intent;
 import android.graphics.Point;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.view.View;
+
+import static android.view.MotionEvent.INVALID_POINTER_ID;
 
 
 public class GameScreen extends AppCompatActivity {
+
+    private static int touchX, touchY;
+    private static int screenWidth, screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_screen);
-        Display mdisp = getWindowManager().getDefaultDisplay();
+        /*Display mdisp = getWindowManager().getDefaultDisplay();
         Point mdispSize = new Point();
         mdisp.getSize(mdispSize);
         int maxX = mdispSize.x;
-        int maxY = mdispSize.y;
-    }
+        int maxY = mdispSize.y;*/
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
-        return true;
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        screenHeight = displayMetrics.heightPixels;
+        screenWidth = displayMetrics.widthPixels;
     }
 
     public void pauseGame(View view){
         Intent intent = new Intent(GameScreen.this, PauseMenu.class);
         startActivity(intent);
-    }
-    public void shopScreen (View view) {
-        Intent intent2 = new Intent(GameScreen.this, ShopScreen.class);
-        intent2.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(intent2);
     }
 
     @Override
@@ -46,6 +50,37 @@ public class GameScreen extends AppCompatActivity {
         super.onPause();
         overridePendingTransition(0,0);
     }
+
+    /*View view = findViewById(android.R.id.content);
+    view.setOnClickListener(new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    };
+
+    public void moveLeft() {
+        ImageView imageView = (ImageView) findViewById(R.id.game);
+        float x = imageView.getX();
+        x-=10;
+        imageView.setX(x);
+    }
+    public void getDistance(){
+        int y,x;
+        int slope;
+        if(touchX > (screenWidth / 2)){
+            x = touchX - (screenWidth / 2);
+            y = screenHeight - touchY;
+        }else{
+            x = (screenWidth / 2) - touchX;
+            y = screenHeight - touchY;
+        }
+
+        slope = y/x;
+    }*/
+
+
 
 }
 
